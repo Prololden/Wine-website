a8e25c57ca9fcadda1845e08f77a69b362e841b5
Prololden <deniz.mamedov2020@mail.ru>
2022-08-30 18:02:02 +0300

    Wine website

---
+++ JS/javascript.js
@@ -0,0 +1,23 @@
+document.getElementById('btn').addEventListener('click', () => {
+    document.getElementById('123').style.display = 'block';
+    document.getElementById('123').style.position = 'relativ';
+});
+document.getElementById('btn_2').addEventListener('click', () => {
+    document.getElementById('123').style.display = 'none';
+});
+
+document.getElementById('btn_5').addEventListener('click', () => {
+    document.getElementById('125').style.display = 'block';
+    document.getElementById('125').style.position = 'relativ';
+});
+document.getElementById('btn_3').addEventListener('click', () => {
+    document.getElementById('125').style.display = 'none';
+});
+
+document.getElementById('btn_245').addEventListener('click', () => {
+    document.getElementById('345').style.display = 'block';
+    document.getElementById('345').style.position = 'relativ';
+});
+document.getElementById('btn_346').addEventListener('click', () => {
+    document.getElementById('345').style.display = 'none';
+});
